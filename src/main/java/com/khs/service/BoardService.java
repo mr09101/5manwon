f6efd749bd6c33905c4f569196ca2571ddb568b7
bicnsuser1 <bicnsuser1@venustas90>
2019-09-11 13:20:58 +0900

    집에서 이어서 할거

---
+++ src/main/java/com/khs/service/BoardService.java
@@ -0,0 +1,15 @@
+package com.khs.service;
+
+import java.util.List;
+
+import com.khs.domain.BoardVO;
+
+public interface BoardService {
+
+	public void regist(BoardVO board) throws Exception;
+	public BoardVO read(Integer bno) throws Exception;
+	public void modify(BoardVO board) throws Exception;
+	public void remove(Integer bno) throws Exception;
+	public List<BoardVO> listAll() throws Exception;
+
+}
